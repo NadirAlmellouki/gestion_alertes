446272dcba29aa511d8172ab6e50b101636cd713
Nadir Almellouki <nadiralmellouki2004@gmail.com>
2026-07-24 15:21:20 +0100

    initial commit de nadir

---
+++ src/main/java/FST/MST_RSI/PFA/ServletInitializer.java
@@ -0,0 +1,13 @@
+package FST.MST_RSI.PFA;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PfaApplication.class);
+	}
+
+}
---
+++ src/test/java/FST/MST_RSI/PFA/PfaApplicationTests.java
@@ -0,0 +1,13 @@
+package FST.MST_RSI.PFA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
